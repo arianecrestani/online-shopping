826f98c7177ae248d9ec72a9563bb489922000d7
Parvaneh Ramezani <parvaneramezani93@gmail.com>
2021-12-07 21:15:57 +0100

    Create function productSection

--- productDetail.js
+++ productDetail.js
@@ -8,7 +8,31 @@ const singleProduct = () => {
         .then((json) => updateUi(json));
 };
 
-const updateUi = (product) => {
+function createProductSection(product) {
+    
+    let divElement = document.createElement("div")
+    divElement.id = 'productSingleSection'
+    let imageDiv = document.createElement("img")
+    imageDiv.innerHTML = product.image
+   /* imageDiv.innerHTML = image.product */
+    imageDiv.id = 'imageOfProduct'
+    divElement.appendChild(imageDiv)
 
-    jjj
+}
+const updateUi = (product) => {
+    let productContainer = document.getElementsByClassName("sproduct")
+
+    product.forEach(product => {
+        const divElement = {
+            title: product.title,
+            image: product.image,
+            // description: product.description,
+            price: product.price,
+            rating: product.rating.rate,
+        }
+        const productSection = createProductSection(divElement);
+        productContainer.appendChild(productSection)
+
+
+    });
 }
