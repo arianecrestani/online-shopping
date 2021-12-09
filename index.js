0f8fc8a3e9eae23df4e9c2a5c9ee0120fa94caae
Ariane Crestani <ariane.mrv@gmail.com>
2021-12-09 19:48:36 +0100

    change de products

--- index.js
+++ index.js
@@ -18,8 +18,6 @@ const btnEletroProducts = () => {
         .then((json) => updateUi(json));
 }
 
-
-
 const createImage = (image) => {
     const imageDiv = document.createElement("div");
     imageDiv.id = "imageProduct";
@@ -59,13 +57,7 @@ const createRating = (rating) => {
     return ratingDiv;
 };
 
-const singleProduct = () => {
-    apiRequest(getSingleProduct())
-        .then((json) => updateUi(json));
-};
-
-
-const createProductSection = (products) => {
+const createProductSection = (product) => {
 
 
     const div = document.createElement("div");
@@ -87,19 +79,19 @@ const createProductSection = (products) => {
     div.appendChild(btnDetails)
 
     
-    const image = createImage(products.image);
+    const image = createImage(product.image);
     div.appendChild(image);
 
-    const title = createTitle(products.title);
+    const title = createTitle(product.title);
     div.appendChild(title);
 
     // const description = createDescription(result.description);
     // div.appendChild(description); // description going to another page (productDetail)
 
-    const price = createPrice(products.price);
+    const price = createPrice(product.price);
     div.appendChild(price);
 
-    const rating = createRating(products.rating);
+    const rating = createRating(product.rating);
     div.appendChild(rating);
 
     return div;
