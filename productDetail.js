69a9c1b83eede7c217cb7f1887a16070b1c80a5d
Parvaneh Ramezani <parvaneramezani93@gmail.com>
2021-12-09 19:14:16 +0100

    Create product items

--- productDetail.js
+++ productDetail.js
@@ -8,6 +8,29 @@ const singleProduct = () => {
 
 
 const updateUiItem = (product) => {
+
     const itemTitle = document.getElementById('itemTitle')
-    itemTitle.innerHTML =  product.title ? product.title : '';
+    itemTitle.innerHTML = product.title ? product.title : '';
+
+    const itemImage = document.getElementById('itemImage')
+    itemImage.innerHTML = product.image ? product.image : '';
+    // imageDiv.innerHTML = `<img src="${image}"/>`;
+
+    const itemPrice = document.getElementById('itemPrice')
+    itemPrice.innerHTML = product.price ? product.price : '';
+
+    const itemRating = document.getElementById('itemRating')
+    itemRating.innerHTML = product.rate ? product.rate : '';
+    
 }
+
+/* const createImage = (image) => {
+    const imageDiv = document.createElement("div");
+    imageDiv.id = "imageProduct";
+    imageDiv.innerHTML = `<img src="${image}"/>`;
+    return imageDiv;
+};
+*/
+
+//localStorage.setItem("")
+
