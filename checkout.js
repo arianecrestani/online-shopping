4295d09e26e02b0b85f2510032bf790e38a109cb
Ariane Crestani <ariane.mrv@gmail.com>
2021-12-13 11:12:35 +0100

    add coment

--- checkout.js
+++ checkout.js
@@ -18,16 +18,16 @@ const loading = () => {
 
 const createTitleDiv = (title) => {
     const titleDiv = document.createElement("div"); // cria uma div 
-    titleDiv.id = "titleProduct"; // criando id para a div
-    titleDiv.innerHTML = title; // mostrando o titulo no html
+    titleDiv.id = "titleProduct"; // create a id to a div
+    titleDiv.innerHTML = title; // show title html
 
-    return titleDiv; // retornado a titlediv
+    return titleDiv; // return titlediv
 };
 
 const createImageDiv = (image) => {
     const imageDiv = document.createElement("img"); // create element div
-    imageDiv.id = "imageProduct"; // criando um id para a div
-    imageDiv.src = image; // colocando a imagem na div 
+    imageDiv.id = "imageProduct"; // create a id to a div
+    imageDiv.src = image; // putting a imagem to div 
     return imageDiv; // retonando a div
 };
 
@@ -36,23 +36,23 @@ const createItemDiv = (product) =>{
     const div = document.createElement("div"); // create element div
     div.id = "createItem"; // create um id para ela
 
-    const title = createTitleDiv(product.title); // criando uma div  e mostrando a title no html
+    const title = createTitleDiv(product.title); // create a div and show title in html
     div.appendChild(title); //colocando o tilte na div
   
-    const image = createImageDiv(product.image); // criando uma img  e mostrando a imagem no html
-    div.appendChild(image); //colocando o image na div
+    const image = createImageDiv(product.image); // create a img and show image in html
+    div.appendChild(image); //puuting image in div
 
-    return div //retoanndo a div
+    return div //retarn a div
 }
 
 const updateUi = (products) => {
-    const itensCar = document.getElementById('itensCar') // pegando div html
-    itensCar.innerHTML= '' // lipando os itens
+    const itensCar = document.getElementById('itensCar') // getting div 
+    itensCar.innerHTML= '' // cleaning itens 
 
-    // para produtos vai retornar um produto
+    // from products return a product 
     products.forEach((product) => {
-        const itemDiv = createItemDiv(product) // criando um produto e mostrando um produto
-        itensCar.appendChild(itemDiv)//adicionando uma item na lista de produtos
+        const itemDiv = createItemDiv(product) //create a product and show the
+        itensCar.appendChild(itemDiv)//add item a product list
     })
    
     
